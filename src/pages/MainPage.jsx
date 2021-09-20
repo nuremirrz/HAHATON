1ce961201398d30c3a62eff635841a85ad7236a4
nuremirrz <nur.emirlan.02@gmail.com>
2021-09-20 19:01:36 +0600

    hahathon

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductTable from '../components/ProductTable';
+
+const MainPage = () => {
+    return (
+        <div>
+            <ProductTable/>
+        </div>
+    );
+};
+
+export default MainPage;
